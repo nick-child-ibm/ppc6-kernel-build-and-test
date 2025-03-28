922802783fe6ab2f97a8baccdcb292d8e0096902
Nick Child <nick.child@ibm.com>
2025-03-28 14:59:01 -0500

    Add custom kernel names
    Signed-off-by: Nick Child <nick.child@ibm.com>

--- run.sh
+++ run.sh
@@ -3,19 +3,24 @@
 LINUX=/home/nchild/IBM/linux-kernel/net-next
 TMPDIR=./linux_tmp
 CONFIG=pseries_le_defconfig
+NAME=
 # IMAGENAME=fedora-ppc64le-linux
 # CONTAINER_SCRIPT=container_script.sh
+
 
 mkdir $TMPDIR
 lndir $LINUX $TMPDIR
 set -e
-# cp $CONTAINER_SCRIPT $TMPDIR/
 
-# podman build -t $IMAGENAME .
-# podman run --platform linux/ppc64le --rm -it --cpus=`nproc` -v $TMPDIR:/`basename $TMPDIR`:z -w /`basename $TMPDIR` \
-# 	-v $LINUX:$LINUX:z  localhost/$IMAGENAME bash -c "./container_script.sh $CONFIG"
-# much more efficient just to cross compile
+# if a kernel name is requested then sub that in, else use the git branch name
+if [ -z $NAME ]; then
+	pushd $LINUX
+	NAME=`git rev-parse --abbrev-ref HEAD`;
+	if [[ $NAME =~ "HEAD" ]]; then NAME=`git describe`; fi;
+	popd
+fi
+
 pushd $TMPDIR
 make ARCH=powerpc CROSS_COMPILE=ppc64le-linux-gnu- -j `nproc` $CONFIG
-make ARCH=powerpc CROSS_COMPILE=ppc64le-linux-gnu- -j `nproc` binrpm-pkg
+make ARCH=powerpc CROSS_COMPILE=ppc64le-linux-gnu- -j `nproc` binrpm-pkg LOCALVERSION=-$NAME
 popd
